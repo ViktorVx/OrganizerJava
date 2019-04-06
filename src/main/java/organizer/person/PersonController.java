2a3420e346cea3282d5581459ee5dbd8dabb70d1
ViktorVx <victorptrv@yandex.ru>
2019-04-06 16:53:19 +0300

    Add i18n-mechanism

--- src/main/java/organizer/person/PersonController.java
+++ src/main/java/organizer/person/PersonController.java
@@ -11,7 +11,7 @@ public class PersonController {
 
     private static final Logger logger = LoggerFactory.getLogger(OrganizerApp.class);
 
-    @RequestMapping("/")
+    @RequestMapping("/new")
     String home() {
         logger.warn("Обновлена главная страница");
         return "Hello world!!!";
