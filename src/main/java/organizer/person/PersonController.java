7beaf3de36ed38b3afb340aa9415f3602c291a27
ViktorVx <victorptrv@yandex.ru>
2019-04-06 15:16:55 +0300

    Add logging-mechanism

--- src/main/java/organizer/person/PersonController.java
+++ src/main/java/organizer/person/PersonController.java
@@ -1,13 +1,19 @@
 package organizer.person;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import organizer.OrganizerApp;
 
 @RestController
 public class PersonController {
 
+    private static final Logger logger = LoggerFactory.getLogger(OrganizerApp.class);
+
     @RequestMapping("/")
     String home() {
+        logger.info("Обновлена главная страница");
         return "Hello world!!!";
     }
 }
