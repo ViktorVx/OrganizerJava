71690078e5ba79ce450bcd46689ab96de703cbc3
ViktorVx <victorptrv@yandex.ru>
2019-04-06 18:45:31 +0300

    Add connection to postgresql database with spring jpa

--- src/main/java/organizer/MvcConfig.java
+++ src/main/java/organizer/MvcConfig.java
@@ -7,11 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "organizer")
@@ -43,4 +47,5 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
     }
+
 }
