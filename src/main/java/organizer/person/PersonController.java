acd885daf6b533a98974af6274700c0898108ce3
ViktorVx <victorptrv@yandex.ru>
2019-04-14 15:54:05 +0300

    Add form for person addition

--- src/main/java/organizer/person/PersonController.java
+++ src/main/java/organizer/person/PersonController.java
@@ -4,14 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import organizer.OrganizerApp;
 
 import java.util.Date;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/persons")
@@ -24,19 +20,33 @@ public class PersonController {
         this.persons = personService;
     }
 
-    @RequestMapping(value = "/new", method = RequestMethod.GET)
-    String addPerson(Model model) {
-        //*** PERSON SAVING ***********************
-        Person person = new Person();
-        person.setFirstName("Петров");
-        person.setLastName("Петр");
-        person.setMiddleName("Петрович");
-        person.setBirthDate(new Date());
-        person.setEmail("petr@ya.ru");
+//    @RequestMapping(value = "/new", method = RequestMethod.GET)
+//    String addPerson(Model model) {
+//        //*** PERSON SAVING ***********************
+//        Person person = new Person();
+//        person.setFirstName("Петров");
+//        person.setLastName("Петр");
+//        person.setMiddleName("Петрович");
+//        person.setBirthDate(new Date());
+//        person.setEmail("petr@ya.ru");
+//
+////        persons.save(person);
+//        //*****************************************
+//        logger.info("Обновлена главная страница");
+//        return "persons/new_person";
+//        //todo следующим этапом сделать форму для ввода персон в БД
+//    }
 
-//        persons.save(person);
-        //*****************************************
-        logger.info("Обновлена главная страница");
-        return "persons/new";
+    @PostMapping("/new_person")
+    public String newPersonSubmit(@ModelAttribute Person person) {
+        persons.save(person);
+        return "persons/additionSuccess";
+    }
+
+    @GetMapping("/new_person")
+    public String newPersonForm(Model model) {
+        model.addAttribute("person", new Person());
+//        System.out.println(((Person)model.asMap().get("person")).getLastName());
+        return "persons/new_person";
     }
 }
